67d85c9d879d78fdddc16eeed2ba80a6a73a791c
alpha-sml <sml.atharva.14@gmail.com>
2025-07-17 01:31:09 +0530

    updated the database

--- property-comparision/src/app/listings/page.js
+++ property-comparision/src/app/listings/page.js
@@ -10,7 +10,7 @@ const ListingsPage = () => {
 
   useEffect(() => {
     fetch(
-      'https://gist.githubusercontent.com/alpha-sml/6cebb505b603d89507b8e4f0d374246f/raw/e39619f4021b41eecbaa1b8c764717a168a10707/properties.json'
+      'https://gist.githubusercontent.com/alpha-sml/6cebb505b603d89507b8e4f0d374246f/raw/014b714c50eef336f348275ab315212d70d98a0c/properties.json'
     )
       .then((res) => res.json())
       .then((data) => setProperties(data))
--- property-comparision/src/components/FeaturedListings/FeaturedListings.js
+++ property-comparision/src/components/FeaturedListings/FeaturedListings.js
@@ -7,7 +7,7 @@ const FeaturedListings = () => {
   const [featuredProperties, setFeaturedProperties] = useState([]);
 
   useEffect(() => {
-    fetch('https://gist.githubusercontent.com/alpha-sml/6cebb505b603d89507b8e4f0d374246f/raw/e39619f4021b41eecbaa1b8c764717a168a10707/properties.json')
+    fetch('https://gist.githubusercontent.com/alpha-sml/6cebb505b603d89507b8e4f0d374246f/raw/014b714c50eef336f348275ab315212d70d98a0c/properties.json')
       .then((res) => res.json())
       .then((data) => setFeaturedProperties(data.slice(0, 5)))
       .catch((err) => console.error('Failed to fetch featured listings:', err));
