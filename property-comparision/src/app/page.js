e66b73a1fb34138460ec6e4f949fbf848995c70b
alpha-sml <sml.atharva.14@gmail.com>
2025-07-17 01:35:08 +0530

    reorganized the home page

--- property-comparision/src/app/page.js
+++ property-comparision/src/app/page.js
@@ -7,8 +7,8 @@ export default function Home() {
   return (
     <div>
       <HeroSection />
-      <FeaturedListings />
       <InfoSection />
+      <FeaturedListings />
       <CTASection />
     </div>
   );
